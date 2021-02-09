346323a2c44bfe4429a7baf226f418e06cfaece6
yasir2000 <yasirkaram@gmail.com>
2021-02-09 17:18:49 +0300

    added UnixProcess function

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -35,6 +36,8 @@ var (
 	name string
 )
 
+
+
 func init() {
 	flag.StringVar(&cert, "cert", "", "give me a certificate")
 	flag.StringVar(&key, "key", "", "give me a key")
@@ -56,6 +59,7 @@ func main() {
 	http.HandleFunc("/", whoamiHandler)
 	http.HandleFunc("/api", apiHandler)
 	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/process", processHandler)
 
 	fmt.Println("Starting up on port " + port)
 
@@ -63,6 +67,42 @@ func main() {
 		log.Fatal(http.ListenAndServeTLS(":"+port, cert, key, nil))
 	}
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+// +build linux
+
+// package ps
+
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"strings"
+// )
+
+// Refresh reloads all the data associated with this process.
+func processHandler(w http.ResponseWriter, p *UnixProcess) Refresh() error {
+	statPath := fmt.Sprintf("/proc/%d/stat", p.pid)
+	dataBytes, err := ioutil.ReadFile(statPath)
+	if err != nil {
+		return err
+	}
+
+	// First, parse out the image name
+	data := string(dataBytes)
+	binStart := strings.IndexRune(data, '(') + 1
+	binEnd := strings.IndexRune(data[binStart:], ')')
+	p.binary = data[binStart : binStart+binEnd]
+
+	// Move past the image name and start parsing the rest
+	data = data[binStart+binEnd+2:]
+	_, err = fmt.Sscanf(data,
+		"%c %d %d %d",
+		&p.state,
+		&p.ppid,
+		&p.pgrp,
+		&p.sid)
+
+	return err
 }
 
 func benchHandler(w http.ResponseWriter, _ *http.Request) {
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/traefik/whoami
 
 go 1.13
 
-require github.com/gorilla/websocket v1.4.2
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/matishsiao/goInfo v0.0.0-20200404012835-b5f882ee2288 // indirect
+	github.com/mitchellh/go-ps v1.0.0 // indirect
+)
